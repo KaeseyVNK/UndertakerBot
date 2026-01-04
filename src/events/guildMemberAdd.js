8f5160305565926a3c076a033b8315cc82d84380
Võ Trung Kiên <128293584+KaeseyVNK@users.noreply.github.com>
2026-01-04 21:30:45 +0700

    Fix role mention formatting in welcome message
    Updated the welcome message to use the correct role mention format by removing the '&' character from the role ID.

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -9,7 +9,7 @@ module.exports = {
         const channel = await member.client.channels.fetch(notifyChannelId);
         if (!channel) return console.error('Notify channel not found');
 
-        const message = `Thay mặt <@&755328143023144971> chào mừng bạn ${member.toString()} đến với server và có khoảng thời gian vui vẻ`;
+        const message = `Thay mặt <@755328143023144971> chào mừng bạn ${member.toString()} đến với server và có khoảng thời gian vui vẻ`;
 
         await channel.send(message);
     },
